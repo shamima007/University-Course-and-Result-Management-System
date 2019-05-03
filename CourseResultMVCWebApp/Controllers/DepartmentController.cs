2341c3fe81ce574c34dcefe10c5b5fe0f8afba8f
shamima007 <48881181+shamima007@users.noreply.github.com>
2019-05-03 17:32:50 +0600

    Add files via upload

---
+++ CourseResultMVCWebApp/Controllers/DepartmentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseResultMVCWebApp.Manager;
+using CourseResultMVCWebApp.Models;
+
+namespace CourseResultMVCWebApp.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        DepartmentManager departmentManager = new DepartmentManager();
+        // GET: /Department/
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Save(Department department)
+        {
+           string message = departmentManager.Save(department);
+            ViewBag.Message = message;
+            return View();
+        }
+        public ActionResult Show()
+        {
+            List<Department> departments = departmentManager.GetAll();
+            ViewBag.Departments = departments;
+            return View();
+        }
+	}
+}
